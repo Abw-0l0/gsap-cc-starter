8c6220743864b0a8ee573358a6435ff7227e18ca
Abdul Basit Iqbal <iamabdulbasit5@gmail.com>
2024-08-12 21:27:14 +0500

    gdsfg

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,11 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+  useGSAP(() => {
+    gsap.fromTo("#red-box", { x: 0, rotate: 0, borderRadius: '0%' }, { x: 250, repeat: -1, yoyo: true, borderRadius: '100%', rotate: 360, duration: 2, ease: 'power1.inOut'})
+  },[])
 
   return (
     <main>
